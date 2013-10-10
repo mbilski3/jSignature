d08a177d1ac7839ca543aba527ad94c0537ce3ea
Mateusz Bilski <mbilski3@gmail.com>
2013-10-10 11:04:08 +0200

    version 0.0.2

--- app/assets/javascripts/jsignature.js
+++ app/assets/javascripts/jsignature.js
@@ -1,12 +1,3 @@
 //= require jSignature.min.js
 //= require jSignature.min.noconflict.js
 //= require flashcanvas.js
-
-$(document).ready(function() {
-    var x = $("#signature");
-    x.jSignature();
-    x.change(function(){
-      $(".sign").val(x.jSignature("getData"));
-    });
-    
-  });
