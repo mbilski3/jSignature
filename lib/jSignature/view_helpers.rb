10fb5fa3f3adb2a3d9d90a6f909f0a19a2e5f5c0
Rhys Park <sales@igpython.co.uk>
2017-01-08 00:35:29 +0000

    Add Format to helper method

--- lib/jSignature/view_helpers.rb
+++ lib/jSignature/view_helpers.rb
@@ -3,25 +3,25 @@ include ActionView::Helpers::TagHelper
 module JSignature
   module ViewHelpers
 
-    def sign_form_field_tag(name)
+    def sign_form_field_tag(name, format)
       return text_field_tag(name,"", :hidden => true)+content_tag(:div,"",:id => "#{name}_sig")+
       content_tag(:script,"$(document).ready(function() {
     var x = $('##{name}_sig');
     x.jSignature();
     x.change(function(){
-      $('##{name}').val(x.jSignature('getData'));
+      $('##{name}').val(x.jSignature('getData', '#{format}'));
     });
     
   });".html_safe)
     end
 
-    def sign_field(name)
+    def sign_field(name, format)
       return text_field(name,:hidden => true)+content_tag(:div, "",:id => "#{name}_sig")+
       content_tag(:script, "$(document).ready(function() {
     var x = $('##{name}_sig');
     x.jSignature();
     x.change(function(){
-      $('##{@object_name}_#{name}').val(x.jSignature('getData'));
+      $('##{@object_name}_#{name}').val(x.jSignature('getData', '#{format}'));
     });
     
   });".html_safe)
